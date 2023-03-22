4566485d9b66be2ebd0c6a35c33e8067c619be8b
filipniestety <127326632+filipniestety@users.noreply.github.com>
2023-03-22 15:58:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 2;
+    int c;
+    c = a + b;
+    System.out.println(c);
+    c = a - b;
+    System.out.println(c);
+    c = a * b;
+    System.out.println(c);
+    c = a / b;
+    System.out.println(c);
+  }
+}
